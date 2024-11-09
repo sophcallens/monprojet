42d3c01a8b4a2104772ae51c497b7fb6c505cc0c
Sophie Callens <soph.callens@gmail.com>
2024-11-09 12:33:57 +0100

    debug

---
+++ 2-vote_par_candidats.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df = pd.read_excel("resultats-par-niveau-cirlg-t1-france-entiere.xlsx")
+
+df1={}
+for column in df.columns : 
+    if column not in {'Code du département','Libellé du département','Code de la circonscription','Inscrits','Votants','Exprimés'} :
+        df1[column] = [((df[column].sum()/df['Inscrits'].sum())*100).round(2)]
+
+print(pd.DataFrame(df1))
