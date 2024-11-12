32c05cb16ce2c46029ecb459d6bcf7ad17454750
Oscar Sans <oscar.sans@etu.minesparis.psl.eu>
2024-11-12 15:56:45 +0100

    nouveau prog

--- 10-Demander_résultat_par_candidat.py
+++ 10-Demander_résultat_par_candidat.py
@@ -4,4 +4,4 @@ df = pd.read_excel("resultats-par-niveau-cirlg-t1-france-entiere.xlsx")
 
 nom=input('De quelle candidats souhaitez vous connaitre le nombre de vote (inscrivez le Prenom NOM)')
 somme = df[nom].sum()
-print(somme)
+print('Le résultat pour', nom, 'est', somme)
