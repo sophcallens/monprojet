5cd3048d9aba78bb792158e21d550d91654349d0
Oscar Sans <oscar.sans@etu.minesparis.psl.eu>
2024-11-14 19:11:59 +0100

    quelques commentaires

--- 09-nb_dinscrit.py
+++ 09-nb_dinscrit.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+#récuperation données
+
 df = pd.read_excel("resultats-par-niveau-cirlg-t1-france-entiere.xlsx")
+
+#calcul somme
 
 somme = df['Inscrits'].sum()
 print(somme)
